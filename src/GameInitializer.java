dc339163c36fd25c3a5be33cd8eba6dcfcb6bff4
procoderer <yunzheng817@gmail.com>
2023-12-09 20:19:23 -0500

    Typo fix

--- src/GameInitializer.java
+++ src/GameInitializer.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class GameInitializer {
     /**
-     * Main method run to start and run the game.
+     * Main method to start and run the game.
      */
     public static void main(String[] args) {
         Runnable game = new main.RunSos();
